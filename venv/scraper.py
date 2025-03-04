70620da24f366ea53c413888e17d84ffc818ebea
Reza <omarsharzil@gmail.com>
2025-03-04 12:28:29 +0600

    variable chromedriver path

--- venv/scraper.py
+++ venv/scraper.py
@@ -5,9 +5,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
 
+import argparse
+parser=argparse.ArgumentParser()
+parser.add_argument('--chromedrive_path', type=str, help="check where is the chromedriver in your pc")
+args=parser.parse_args()
+
 def main():
     url = "https://calmatters.org/california-wildfire-map-tracker/?gad_source=1&gclid=Cj0KCQiAy8K8BhCZARIsAKJ8sfS4CG-Qk4fQSwk5ySvizY-3O19Q8aoNgLVKRs9YyAmd-QK2evxiS-saAgVBEALw_wcB"
-    webdriver_path = "C:\\Program Files (x86)\\chromedriver.exe"  # Ensure this path uses double backslashes
+    webdriver_path = parser.chromedrive_path  # Ensure this path uses double backslashes
     service = Service(webdriver_path)
     driver = webdriver.Chrome(service=service)
     driver.get(url)
